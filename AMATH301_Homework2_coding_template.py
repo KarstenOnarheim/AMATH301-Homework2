f2c483cb6c22b6dd6c8d320e98c712a4017eec0d
KarstenOnarheim <onarhk@gmail.com>
2023-01-18 10:57:12 -0800

    Update AMATH301_Homework2_coding_template.py

--- AMATH301_Homework2_coding_template.py
+++ AMATH301_Homework2_coding_template.py
@@ -72,7 +72,7 @@ A12 = Fibonacci[:n+1]
 ## Part a
 # I will let you create x here.
 x = np.linspace(-1*np.pi, np.pi, 100)
-A13 = x.copy
+A13 = np.copy(x)
 
 ## Part b
 # Assuming that x is defined correctly above, now we can just do regular
